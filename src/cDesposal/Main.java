f93707d9726e513c4b89d7c7c871ddb370c672bf
BuildTools <unconfigured@null.spigotmc.org>
2019-07-14 20:48:34 +0200

    First commit

---
+++ src/cDesposal/Main.java
@@ -0,0 +1,117 @@
+package cDesposal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.block.DoubleChest;
+import org.bukkit.inventory.DoubleChestInventory;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Main extends JavaPlugin {
+	public Map<Location, String> disposalSignsOwner = new HashMap<Location, String>();
+	public List<Location> disposalSigns = new ArrayList<>();
+
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(new listeners.listener(), (Plugin) this);
+		getSavedDisposalSigns();
+		emptyChests();
+	}
+
+	public void onDisable() {
+		List<String> s = new ArrayList<String>();
+
+		for (int i = 0; i < disposalSigns.size(); i++) {
+			if(!s.contains(disposalSigns.get(i).toString())) {
+				s.add(disposalSigns.get(i).toString());
+			}
+		}
+		getConfig().set("disposalSigns", s);
+
+		List<String> s2 = new ArrayList<String>();
+		for (Location loc : disposalSignsOwner.keySet()) {
+			s2.add(loc.toString() + ":" + disposalSignsOwner.get(loc));
+		}
+		getConfig().set("disposalSignsOwner", s2);
+		saveConfig();
+	}
+
+	public void getSavedDisposalSigns() {
+		List<String> s = this.getConfig().getStringList("disposalSigns");
+
+		for (String str : s) {
+			String[] arg = str.split(",");
+			arg[0] = arg[0].replace("Location", "");
+			arg[0] = arg[0].replace("{world=CraftWorld{name=", "");
+			arg[0] = arg[0].replace("}", "");
+			arg[1] = arg[1].replace("x=", "");
+			arg[2] = arg[2].replace("y=", "");
+			arg[3] = arg[3].replace("z=", "");
+			arg[4] = arg[4].replace("pitch=", "");
+			arg[5] = arg[5].replace("yaw=", "");
+			arg[5] = arg[5].replace("}", "");
+
+			Location loc = new Location(Bukkit.getWorld(arg[0]), Double.parseDouble(arg[1]), Double.parseDouble(arg[2]),
+					Double.parseDouble(arg[3]), Float.parseFloat(arg[4]), Float.parseFloat(arg[5]));
+			disposalSigns.add(loc);
+
+		}
+
+		List<String> s2 = this.getConfig().getStringList("disposalSignsOwner");
+
+		for (String str : s2) {
+			String[] words = str.split(":");
+
+			String[] arg = words[0].split(",");
+			arg[0] = arg[0].replace("Location", "");
+			arg[0] = arg[0].replace("{world=CraftWorld{name=", "");
+			arg[0] = arg[0].replace("}", "");
+			arg[1] = arg[1].replace("x=", "");
+			arg[2] = arg[2].replace("y=", "");
+			arg[3] = arg[3].replace("z=", "");
+			arg[4] = arg[4].replace("pitch=", "");
+			arg[5] = arg[5].replace("yaw=", "");
+			arg[5] = arg[5].replace("}", "");
+
+			Location loc = new Location(Bukkit.getWorld(arg[0]), Double.parseDouble(arg[1]), Double.parseDouble(arg[2]),
+					Double.parseDouble(arg[3]), Float.parseFloat(arg[4]), Float.parseFloat(arg[5]));
+			disposalSignsOwner.put(loc, words[1]);
+
+		}
+	}
+
+	public void emptyChests() {
+		new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < disposalSigns.size(); i++) {
+					if (disposalSigns.get(i).getBlock().getType().equals(Material.WALL_SIGN)) {
+						org.bukkit.material.Sign s = (org.bukkit.material.Sign) disposalSigns.get(i).getBlock()
+								.getState().getData();
+						Block attachedBlock = disposalSigns.get(i).getBlock().getRelative(s.getAttachedFace());
+						if (attachedBlock.getType().equals(Material.CHEST)
+								|| attachedBlock.getType().equals(Material.TRAPPED_CHEST)) {
+							Chest c = (Chest) attachedBlock.getState();
+							if (c.getInventory() instanceof DoubleChestInventory) {
+								DoubleChest doubleChest = (DoubleChest) c.getInventory().getHolder();
+								doubleChest.getInventory().clear();
+							} else {
+								c.getInventory().clear();
+							}
+						}
+					}
+				}
+			}
+
+		}.runTaskTimerAsynchronously(this, 0, 1200);
+	}
+}
---
+++ src/listeners/listener.java
@@ -0,0 +1,102 @@
+package listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
+
+import cDesposal.Main;
+
+public class listener implements Listener {
+
+	Main pl = Main.getPlugin(Main.class);
+
+	 @EventHandler
+	 public void diamonPlace(BlockPlaceEvent e) {
+	 if (e.getBlock().getType().equals(Material.DIAMOND_BLOCK)) {
+	 Bukkit.getServer().broadcastMessage(pl.disposalSignsOwner.toString());
+	 }
+	 }
+
+	@EventHandler
+	public void signPlace(SignChangeEvent e) {
+		if (e.getBlock().getType() == Material.WALL_SIGN) {
+			org.bukkit.material.Sign s = (org.bukkit.material.Sign) e.getBlock().getState().getData();
+			if (!e.getLine(0).toLowerCase().equals("[cdisposal]")) {
+				return;
+			}
+			Block attachedBlock = e.getBlock().getRelative(s.getAttachedFace());
+			if (!attachedBlock.getLocation().getBlock().getType().equals(Material.CHEST)
+					& !attachedBlock.getLocation().getBlock().getType().equals(Material.TRAPPED_CHEST)) {
+				return;
+			}
+			// Check if chest is empty
+			// Chest c = (Chest) attachedBlock.getState();
+			// if (c.getInventory() instanceof DoubleChestInventory) {
+			// DoubleChest doubleChest = (DoubleChest) c.getInventory().getHolder();
+			// for (ItemStack item : doubleChest.getInventory().getContents()) {
+			// if (item != null) {
+			// e.getPlayer().sendMessage("§4The chest needs to be empty!");
+			// return;
+			// }
+			// }
+			// } else {
+			// for (ItemStack item : c.getInventory().getContents()) {
+			// if (item != null) {
+			// e.getPlayer().sendMessage("§4The chest needs to be empty!");
+			// return;
+			// }
+			// }
+			// }
+
+			e.setLine(0, "§8[§2cDisposal§8]");
+			e.getPlayer().sendMessage("§8[§2cDisposal§8] §aDu har skapat en §2cDisposal§a-skylt med framgång!");
+			pl.disposalSigns.add(e.getBlock().getLocation());
+			pl.disposalSignsOwner.put(e.getBlock().getLocation(), e.getPlayer().getUniqueId().toString());
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onSignBreak(BlockBreakEvent e) {
+		BlockFace[] bf = new BlockFace[] { BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.NORTH };
+		Block b;
+		for (int i = 3; i >= 0; i--) {
+			b = e.getBlock().getRelative(bf[(i)]);
+			if (b.getType() == Material.WALL_SIGN) {
+				org.bukkit.material.Sign signStateData = (org.bukkit.material.Sign) b.getState().getData();
+				Block attachedBlock = b.getRelative(signStateData.getAttachedFace());
+				// Check if sign is facing against same block as e.getblock.
+				if (attachedBlock.getLocation().equals(e.getBlock().getLocation())) {
+					org.bukkit.block.Sign signState = (org.bukkit.block.Sign) b.getState();
+					if (signState.getLine(0).equals("§8[§2cDisposal§8]")) {
+						if (e.isCancelled()) {
+							return;
+						}
+						pl.disposalSigns.remove(signState.getBlock().getLocation());
+						pl.disposalSignsOwner.remove(signState.getBlock().getLocation());
+
+					}
+				}
+			}
+		}
+
+		if (!e.getBlock().getType().equals(Material.WALL_SIGN)) {
+			return;
+		}
+		org.bukkit.block.Sign s = (org.bukkit.block.Sign) e.getBlock().getState();
+		if (!s.getLine(0).equals("§8[§2cDisposal§8]")) {
+			return;
+		}
+		if(e.isCancelled()) {
+			return;
+		}
+		pl.disposalSigns.remove(e.getBlock().getLocation());
+		pl.disposalSignsOwner.remove(e.getBlock().getLocation());
+	}
+}
